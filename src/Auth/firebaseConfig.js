ba6b446caddce5cd9ad84974be1c3bf9a071b097
shemuls <61226366+shemuls@users.noreply.github.com>
2021-08-07 08:19:43 +0600

    Delete firebaseConfig.js

--- src/Auth/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCSr_4hHQsOskUr1J922bJI--2-Ew_65rI",
-  authDomain: "fast-rider-ltd.firebaseapp.com",
-  projectId: "fast-rider-ltd",
-  storageBucket: "fast-rider-ltd.appspot.com",
-  messagingSenderId: "26619534126",
-  appId: "1:26619534126:web:955bc315b529bc4a2478bf",
-};
